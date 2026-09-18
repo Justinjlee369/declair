3f19b6a8d6966b1e19b0f4ba88d9d6aa55399172
Justinjlee369 <103040782+Justinjlee369@users.noreply.github.com>
2026-09-18 17:21:36 +0100

    Add Atlassian setup route

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Register from '@/pages/Register';
 import ForgotPassword from '@/pages/ForgotPassword';
 import ResetPassword from '@/pages/ResetPassword';
 import Home from '@/pages/Home';
+import AtlassianSetup from '@/pages/AtlassianSetup';
 // Add page imports here
 
 const AuthenticatedApp = () => {
@@ -47,6 +48,7 @@ const AuthenticatedApp = () => {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route element={<ProtectedRoute unauthenticatedElement={<Navigate to="/login" replace />} />}>
         <Route path="/" element={<Home />} />
+        <Route path="/settings/atlassian" element={<AtlassianSetup />} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
